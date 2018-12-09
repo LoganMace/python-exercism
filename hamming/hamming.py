08f5bcd25c4fa76faab2f1028997b346edf4b0fc
Logan Mace <lmmace92@hotmail.com>
2018-12-08 22:40:16 -0600

    hamming finished

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,15 @@
 def distance(strand_a, strand_b):
-    pass
+  hamming = 0
+  if len(strand_a) == 0 and len(strand_b) == 0:
+    return 0
+  elif len(strand_a) == len(strand_b):
+    for i, lett in enumerate(strand_a):
+      if lett != strand_b[i]:
+        hamming += 1
+  elif len(strand_a) > len(strand_b):
+    raise ValueError('Strand A is too long!')
+  elif len(strand_a) < len(strand_b):
+    raise ValueError('Strand B is too long!')
+  return hamming
+
+
